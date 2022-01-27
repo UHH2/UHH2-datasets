a2a505f34ca37bdfe44437d0de7597c063a15bd5
Daniel Hundhausen <33727991+danielhundhausen@users.noreply.github.com>
2022-01-27 15:29:47 +0100

    remove deprecated verbose parameter from namedtple
    verbose is no longer a kwarg since Python 3.7

--- CrossSectionHelper.py
+++ CrossSectionHelper.py
@@ -1,7 +1,7 @@
 from collections import namedtuple,Mapping
 
 def namedtuple_with_defaults(typename, field_names, default_values=()):
-    T = namedtuple(typename, field_names, verbose=False)
+    T = namedtuple(typename, field_names)
     T.__new__.__defaults__ = (None,) * len(T._fields)
     if isinstance(default_values, Mapping):
         prototype = T(**default_values)
